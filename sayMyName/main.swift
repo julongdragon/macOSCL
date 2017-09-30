c95327868bfacfe64b27d536bbd349ca33385f85
Eakkasit Tunsakool <eakkasittunsakool@Eakkasits-iMac.local>
2017-09-30 19:01:54 +0700

    init

---
+++ sayMyName/main.swift
@@ -0,0 +1,18 @@
+//
+//  main.swift
+//  sayMyName
+//
+//  Created by Eakkasit Tunsakool on 9/30/2560 BE.
+//  Copyright © 2560 Eakkasit Tunsakool. All rights reserved.
+//
+
+import Foundation
+
+print("Say my name:")
+do {
+    let sayMyName = readLine()
+    let myName : String? = sayMyName
+    print("\(myName!)")
+}
+
+
